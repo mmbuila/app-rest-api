871788aa4988892ccac69a78ed044850eb6da191
Michel MBUILA <Michel.mbuila@gmail.com>
2021-02-11 18:18:14 +0100

    Initialize commit

---
+++ src/main/java/org/chembotula/app/AppRestApiApplication.java
@@ -0,0 +1,13 @@
+package org.chembotula.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestApiApplication.class, args);
+	}
+
+}
